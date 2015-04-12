3b4c4ef4ab09ebfe03074370b53eedd4aa3a3e27
Len <len@len-ThinkPad-T430.(none)>
2015-04-12 15:44:45 -0500

    style changes

--- app/src/main/java/com/imoandroid/imoandroidapp/RxFragment.java
+++ app/src/main/java/com/imoandroid/imoandroidapp/RxFragment.java
@@ -22,10 +22,10 @@ public class RxFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_foundation, container, false);
-        TextView tv = (TextView) v.findViewById(R.id.fragment_textView);
-        Button btnSearch = (Button) v.findViewById(R.id.btnSearch);
-        tv.setText(this.getTag() + " Content");
+        View v = inflater.inflate(R.layout.fragment_rx, container, false);
+        //TextView tv = (TextView) v.findViewById(R.id.fragment_textView);
+        Button btnSearch = (Button) v.findViewById(R.id.btnAdd);
+        //tv.setText(this.getTag() + " Content");
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/imoandroid/imoandroidapp/TxFragment.java
+++ app/src/main/java/com/imoandroid/imoandroidapp/TxFragment.java
@@ -23,10 +23,10 @@ public class TxFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_foundation, container, false);
-        TextView tv = (TextView) v.findViewById(R.id.fragment_textView);
-        Button btnSearch = (Button) v.findViewById(R.id.btnSearch);
-        tv.setText(this.getTag() + " Content");
+        View v = inflater.inflate(R.layout.fragment_tx, container, false);
+       // TextView tv = (TextView) v.findViewById(R.id.fragment_textView);
+        Button btnSearch = (Button) v.findViewById(R.id.btnAdd);
+      //  tv.setText(this.getTag() + " Content");
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
