4c63b865f3b1d8f7462533476d8c2664c1ea725a
Namrata Prabhu <namrataprabhu@wirelessprvnat-172-17-192-122.near.illinois.edu>
2015-04-12 12:44:04 -0500

    changed tester

--- app/src/main/java/com/imoandroid/imoandroidapp/APICaller/tester.java
+++ app/src/main/java/com/imoandroid/imoandroidapp/APICaller/tester.java
@@ -1,9 +1,6 @@
 package com.imoandroid.imoandroidapp.APICaller;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+
 
 import org.json.JSONObject;
 
@@ -14,27 +11,7 @@ import java.util.Map;
  * Created by mayoorrai on 4/1/15.
  */
 public class tester {
-    public static void main (String[] args) {
-//        ArrayList<Map> temp = APICaller.TxGET("arm", 23);
-//        for(int i = 0; i < temp.size(); i++) {
-//            System.out.println(temp.get(i));
-//        }
 
-       try{
-           System.out.println("Hello");
-           HttpResponse<JsonNode> response = Unirest.get("http://httpbin.org/{method}")
-                   .routeParam("method", "get")
-                   .queryString("name", "Mark")
-                   .asJson();
-
-           System.out.println(response.toString());
-
-        }
-        catch (UnirestException e){
-
-            e.printStackTrace();
-        };
-    }
 
 
 }
