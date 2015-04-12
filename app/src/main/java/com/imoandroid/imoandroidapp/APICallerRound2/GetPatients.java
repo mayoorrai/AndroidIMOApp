8daa217cf25f833fae203f7e73cce8d4b1be19be
Sammy <samarth.k.chopra@gmail.com>
2015-04-12 13:40:25 -0500

    *

---
+++ app/src/main/java/com/imoandroid/imoandroidapp/APICallerRound2/GetPatients.java
@@ -0,0 +1,40 @@
+package com.imoandroid.imoandroidapp.APICallerRound2;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+/**
+ * Created by samarthchopra on 4/12/15.
+ */
+public class GetPatients extends AsyncTask<String, Integer, HttpResponse<JsonNode>> {
+
+    protected HttpResponse<JsonNode> doInBackground(String... msg) {
+
+        HttpResponse<JsonNode> request = null;
+        try {
+            request = Unirest.get("http://66.252.70.193/{method}")
+                                .routeParam("method","patients")
+                                .queryString("apiKey","QhIno484vsazggxvZgf2EGZjkYunH24f7MNz5JXmI83bDMTOgmwVw6eqss7I18U7")
+                                .asJson();
+            Log.v("***-", request.getBody().toString());
+        } catch (UnirestException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return request;
+    }
+
+    protected void onProgressUpdate(Integer...integers) {
+    }
+    protected void onPostExecute(HttpResponse<JsonNode> response) {
+        String answer = response.getBody().toString();
+//        TextView txtView = (TextView) findViewById(R.id.textView1);
+//        txtView.setText(answer);
+    }
+}
